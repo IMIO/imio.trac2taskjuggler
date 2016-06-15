917ed3aec77df11342948179e6d561bf8afeb86c
Stephan Geulette <s.geulette@imio.be>
2016-06-15 13:17:53 +0200

    Split TELE project

--- imio/trac2taskjuggler/config.py
+++ imio/trac2taskjuggler/config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-PROJECTS = ('CARTO', 'DMS', 'INFRA', 'INTRA', 'MOD', 'PM', 'PRJ', 'SITE', 'TELE', 'URBAN', 'URBDIAL')
+PROJECTS = ('CARTO', 'DMS', 'INFRA', 'INTRA', 'MOD', 'PM', 'PRJ', 'SITE', 'TELE1', 'TELE2', 'URBAN', 'URBDIAL')
 TRAC_URL = "http://trac.imio.be/trac"
 TICKET_URL = "%s/ticket" % TRAC_URL
