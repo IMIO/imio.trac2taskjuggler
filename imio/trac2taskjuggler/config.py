af87787c7174b7558d3a804ff26408eae9dfb6d9
Stephan Geulette <s.geulette@imio.be>
2016-08-22 13:38:51 +0200

    Added TELE2 as kept project

--- imio/trac2taskjuggler/config.py
+++ imio/trac2taskjuggler/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 PROJECTS = ('CARTO', 'DMS', 'INFRA', 'INTRA', 'MOD', 'PM', 'PRJ', 'SITE', 'TELE1', 'TELE2', 'URBAN', 'URBDIAL')
-PROJECTS_TO_KEEP = ('DMS', 'MOD', 'PM', 'PRJ')
+PROJECTS_TO_KEEP = ('DMS', 'MOD', 'PM', 'PRJ', 'TELE2')
 TRAC_URL = "http://trac.imio.be/trac"
 TICKET_URL = "%s/ticket" % TRAC_URL
