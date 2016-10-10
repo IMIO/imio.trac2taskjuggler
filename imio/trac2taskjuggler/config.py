a1c9824cdb6c0f832cf4fe53f280c41728b53888
Stephan Geulette <s.geulette@imio.be>
2016-10-10 13:53:34 +0200

    Added SITE and URBAN projects

--- imio/trac2taskjuggler/config.py
+++ imio/trac2taskjuggler/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 PROJECTS = ('CARTO', 'DMS', 'INFRA', 'INTRA', 'MOD', 'PM', 'PRJ', 'SITE', 'TELE1', 'TELE2', 'URBAN', 'URBDIAL')
-PROJECTS_TO_KEEP = ('DMS', 'MOD', 'PM', 'PRJ', 'TELE2')
+PROJECTS_TO_KEEP = ('DMS', 'MOD', 'PM', 'PRJ', 'SITE', 'TELE2', 'URBAN')
 TRAC_URL = "http://trac.imio.be/trac"
 TICKET_URL = "%s/ticket" % TRAC_URL
